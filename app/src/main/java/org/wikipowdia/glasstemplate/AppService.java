bf2a4d1c8409c470362e9c023bee869391cfb952
powderly <powderly@fffff.at>
2015-01-01 23:39:42 +0900

    first dump

---
+++ app/src/main/java/org/wikipowdia/glasstemplate/AppService.java
@@ -0,0 +1,57 @@
+package org.wikipowdia.glasstemplate;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+
+import com.google.android.glass.timeline.LiveCard;
+import com.google.android.glass.timeline.LiveCard.PublishMode;
+
+/****************************************
+ * creates the live card service intent
+ * and handles the condition when it is destroyed
+ *
+ *
+ *
+ */
+
+public class AppService extends Service {
+    private static final String TAG = "AppService";
+    private static final String LIVE_CARD_ID = "HelloGlass";
+    private AppDrawer mCallback;
+    private LiveCard mLiveCard;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (mLiveCard == null) {
+            mLiveCard = new LiveCard(this, LIVE_CARD_ID);
+            mCallback = new AppDrawer(this);
+            mLiveCard.setDirectRenderingEnabled(true).getSurfaceHolder().addCallback(mCallback);
+            Intent menuIntent = new Intent(this, MenuActivity.class);
+            mLiveCard.setAction(PendingIntent.getActivity(this, 0, menuIntent, 0));
+            mLiveCard.publish(PublishMode.REVEAL);
+        }
+        return START_STICKY;
+    }
+    @Override
+    public void onDestroy() {
+        if (mLiveCard != null && mLiveCard.isPublished()) {
+            if (mCallback != null) {
+                mLiveCard.getSurfaceHolder().removeCallback(mCallback);
+            }
+            mLiveCard.unpublish();
+            mLiveCard = null;
+        }
+        super.onDestroy();
+    }
+}
